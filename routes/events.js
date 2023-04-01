87a4e095054e09c874c16f6feadf0fc9302d5b2c
Rafa <archisoftes@gmail.com>
2023-04-01 12:42:42 +0200

    primer commit

---
+++ routes/events.js
@@ -0,0 +1,40 @@
+const { Router } = require('express')
+const router = Router();
+const { check } = require('express-validator');
+const { validarCampos } = require ('../middlewares/validar-campos');
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento} = require('../controllers/events');
+const {validarJWT} = require('../middlewares/validar-jwt');
+const { isDate } = require('../helpers/idDate');
+
+
+/* Event Routes
+    /api/events
+*/
+
+//Todas tienen que pasar por la validación del JWT
+router.use( validarJWT );
+
+
+//Obtener eventos
+router.get('/', getEventos )
+//router.get('/renew', validarJWT, revalidarToken);
+
+
+//Crear un nuevo evento
+router.post('/',  [
+    check('title', 'El título es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom( isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom( isDate),
+    validarCampos
+],crearEvento)
+
+
+//Actualizar evento
+router.put('/:id', actualizarEvento)
+
+//Borrar evento
+router.delete('/:id', eliminarEvento)
+
+
+
+module.exports = router;
